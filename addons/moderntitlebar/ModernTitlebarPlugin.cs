08412960d02c6872ac8a98db98b9d7f4600ba124
MarioBossReal <peternajjar2002@gmail.com>
2026-02-04 17:23:35 +1100

    Change Enter/Exit Tree to Enable/Disable Plugin
    fixes disabling of the plugin before editor exit which caused the default titlebar setup to show as the editor is shutting down

--- addons/moderntitlebar/ModernTitlebarPlugin.cs
+++ addons/moderntitlebar/ModernTitlebarPlugin.cs
@@ -66,7 +66,7 @@ public partial class ModernTitlebarPlugin : EditorPlugin, ISerializationListener
     Control.SizeFlags MenuBarSizeFlagsV;
     Control.SizeFlags RunBarSizeFlagsV;
 
-    public override void _EnterTree()
+    public override void _EnablePlugin()
 	{
 		if (!OperatingSystem.IsWindows())
 			return;
@@ -162,7 +162,7 @@ public partial class ModernTitlebarPlugin : EditorPlugin, ISerializationListener
 		OnWindowSizeChanged();
     }
 
-	public override void _ExitTree()
+	public override void _DisablePlugin()
 	{
 		if (!OperatingSystem.IsWindows())
 			return;
